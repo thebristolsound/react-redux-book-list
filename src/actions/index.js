769870de9938d60897c2d4def49d53c4fcb84d84
Matt Donovan <mattddonovan@gmail.com>
2018-10-09 05:37:44 -0500

    Setting up actions for dispatch through reducers

---
+++ src/actions/index.js
@@ -0,0 +1,3 @@
+export function selectBook(book) {
+  console.log('A book as been selected:', book.title);
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: '2001: A Space Odyssey' },
+    { title: 'The Shining' },
+    { title: 'Dr. Strangelove' },
+    { title: 'Encino Man' },
+  ];
+}
