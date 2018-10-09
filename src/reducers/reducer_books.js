9c8a063c898812f6b0dcc16ec591361a0b3785d5
Matt Donovan <mattddonovan@gmail.com>
2018-10-09 06:42:57 -0500

    Final changes to reducers, completed project

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(state, action) {
   return [
-    { title: '2001: A Space Odyssey' },
-    { title: 'The Shining' },
-    { title: 'Dr. Strangelove' },
-    { title: 'Encino Man' },
+    { title: '2001: A Space Odyssey', pages: 2001 },
+    { title: 'The Shining', pages: 237},
+    { title: 'Dr. Strangelove', pages: 1960 },
+    { title: 'Encino Man', pages: 420 }
   ];
 }
